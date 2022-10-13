48ec8093fb6d97c5308c25552eba571ea3adbb3b
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-10-13 15:46:52 +0200

    main activity elkezdve

--- app/src/main/java/com/example/coinflip/MainActivity.java
+++ app/src/main/java/com/example/coinflip/MainActivity.java
@@ -3,9 +3,19 @@ package com.example.coinflip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ImageView coinsPlace;
+    private Button fejGomb;
+    private Button irasGomb;
+    private TextView dontetlenText;
+    private TextView gyozelemText;
+    private TextView veresegtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
